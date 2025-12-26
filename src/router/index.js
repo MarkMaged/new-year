301bf76694f207b3ec43fdb99b058e70f963b6ad
Mark Maged <markmaged399@gmail.com>
2025-12-26 14:33:31 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//   ],
+// })
+
+// export default router
+
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  // باقي المسارات
+]
+
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  ],
+  history: createWebHashHistory(), // ✅ مهم: hash mode
+  routes,
 })
 
 export default router
